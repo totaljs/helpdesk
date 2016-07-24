f8168fd731f4f4a4ebe8111cfe5190b4d627e120
Peter Širka <petersirka@gmail.com>
2016-07-24 18:48:40 +0200

    Updated script.

--- postgresql.sql
+++ postgresql.sql
@@ -109,6 +109,58 @@ WITH (
   OIDS=FALSE
 );
 
+
+CREATE OR REPLACE VIEW public.view_ticket AS
+ SELECT a.id,
+    a.name,
+    a.project,
+    a.search,
+    a.labels,
+    a.minutes,
+    b.name AS "user",
+    b.email,
+    b.language,
+    b.photo,
+    b.company,
+    a.iduser,
+    a.iduserlast,
+    a.idsolver,
+    a.issolved,
+    a.ispriority,
+    b.isnotification,
+    a.datecreated,
+    a.dateupdated,
+    b.minutes AS minutesuser,
+    a.idsolution,
+    b."position",
+    a.countcomments
+   FROM tbl_ticket a
+     JOIN tbl_user b ON b.id::text = a.iduser::text
+  WHERE a.isremoved = false;
+
+
+CREATE OR REPLACE VIEW public.view_ticket_comment AS
+ SELECT a.id,
+    a.iduser,
+    a.idparent,
+    a.idticket,
+    a.body,
+    b.name AS "user",
+    b.email,
+    b.photo,
+    b.language,
+    a.isoperation,
+    a.datecreated,
+    a.dateupdated,
+    b.company,
+    b.isnotification,
+    a.issolution AS issolved,
+    a.operation,
+    b."position"
+   FROM tbl_ticket_comment a
+     JOIN tbl_user b ON a.iduser::text = b.id::text
+  WHERE a.isremoved = false;
+
 -- DEFAULT ADMINISTRATOR
 -- login: support@totaljs.com
 -- password: 123456
