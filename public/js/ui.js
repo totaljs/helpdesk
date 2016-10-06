cad93c1acfd4e6e6a1270622f7c22b8f1f698c7b
Peter Širka <petersirka@gmail.com>
2016-10-06 12:28:13 +0200

    Fix UI - checkboxlist.

--- public/js/ui.js
+++ public/js/ui.js
@@ -1382,10 +1382,8 @@ COMPONENT('repeater-group', function() {
 
 COMPONENT('checkboxlist', function() {
 	var self = this;
-	var template = Tangular.compile('<div class="{0} ui-checkboxlist-checkbox"><label><input type="checkbox" value="{{ id }}"><span>{{ name }}</span></label></div>'.format(self.attr('data-class')));
-
+	var template = Tangular.compile('<div class="{0} ui-checkboxlist-checkbox"><div><label><input type="checkbox" value="{{ id }}"><span>{{ name }}</span></label></div></div>'.format(self.attr('data-class')));
 	self.make = function() {
-
 		self.element.on('click', 'input', function() {
 			var arr = self.get() || [];
 			var value = self.parser(this.value);
@@ -1395,53 +1393,44 @@ COMPONENT('checkboxlist', function() {
 			else
 				arr.splice(index, 1);
 			self.set(arr);
+			self.change(true);
 		});
-
 		self.element.on('click', '.ui-checkboxlist-selectall', function() {
 			var arr = [];
 			var inputs = self.element.find('input');
 			var value = self.get();
-
 			if (value && inputs.length === value.length) {
 				self.set(arr);
+				self.change(true);
 				return;
 			}
-
 			inputs.each(function() {
 				arr.push(self.parser(this.value));
 			});
-
 			self.set(arr);
+			self.change(true);
 		});
-
 		self.make = function() {
-
 			var options = self.attr('data-options');
 			if (!options)
 				return;
-
 			var arr = options.split(';');
 			var datasource = [];
-
 			for (var i = 0, length = arr.length; i < length; i++) {
 				var item = arr[i].split('|');
 				datasource.push({ id: item[1] === undefined ? item[0] : item[1], name: item[0] });
 			}
-
 			self.redraw(datasource);
 		};
-
 		self.setter = function(value) {
 			self.element.find('input').each(function() {
 				this.checked = value && value.indexOf(self.parser(this.value)) !== -1;
 			});
 		};
-
 		self.redraw = function(arr) {
 			var builder = [];
 			var kn = self.attr('data-source-text') || 'name';
 			var kv = self.attr('data-source-value') || 'id';
-
 			for (var i = 0, length = arr.length; i < length; i++) {
 				var item = arr[i];
 				if (typeof(item) === 'string')
@@ -1449,15 +1438,13 @@ COMPONENT('checkboxlist', function() {
 				else
 					builder.push(template({ id: item[kv] === undefined ? item[kn] : item[kv], name: item[kn] }));
 			}
-
 			if (!builder.length)
 				return;
-
-			builder.push('<div class="clearfix"></div><div class="col-md-12"><div class="ui-checkboxlist-selectall"><a href="javascript:void(0)"><i class="fa fa-object-group mr5"></i>{0}</a></div></div>'.format(self.attr('data-button')));
+			builder.push('<div class="clearfix"></div>');
+			self.attr('data-button') && builder.push('<div class="col-md-12"><div class="ui-checkboxlist-selectall"><a href="javascript:void(0)"><i class="fa fa-object-group mr5"></i>{0}</a></div></div>'.format(self.attr('data-button')));
 			self.html(builder.join(''));
 			return self;
 		};
-
 		var datasource = self.attr('data-source');
 		if (datasource) {
 			self.watch(datasource, function(path, value) {
