8c195d1add3b4277e7256331f65cba5db40ddd2c
pooya <pooya_azarpour@yahoo.com>
2018-05-03 00:50:45 +0430

    Add settings options, gitlab oauth2 login

--- controllers/api.js
+++ controllers/api.js
@@ -1,3 +1,6 @@
+const Async = require('async');
+const Request = require('request');
+
 exports.install = function() {
 	// Tickets
 	F.route('/api/tickets/',              json_query,            ['authorize', '*Ticket']);
@@ -34,6 +37,12 @@ exports.install = function() {
 	F.route('/api/login/',                json_exec,             ['unauthorize', '*Login', 'post']);
 	F.route('/api/password/',             json_exec,             ['unauthorize', '*Password', 'post']);
 	F.route('/api/token/',                json_exec,             ['unauthorize', '*Token', 'post']);
+
+    // Settings
+    F.route('/api/settings/',              json_settings,        ['authorize', '*Labels']);
+    F.route('/api/settings/', 			   json_save_settings,   ['authorize', '*Settings', 'post']);
+    F.route('/api/settings/{type}/{name}/',json_remove_settings, ['authorize', '*Settings', 'delete']);
+    F.route('/api/project/external/', 	   json_external_project,['authorize', 'json', 'post']);
 };
 
 function json_cdl() {
@@ -61,6 +70,152 @@ function json_cdl() {
 		response.languages = response.languages.map(n => n.name);
 		self.json(response);
 	});
+}
+
+function json_settings() {
+    let self = this;
+
+    if (!self.user.isadmin)
+        return self.invalid().push('error-privileges');
+
+    let sql = DB();
+
+    sql.select('labels', 'cdl_label');
+    sql.select('projects', 'cdl_project');
+    sql.select('settings', 'cdl_settings');
+
+    sql.exec(function(err, response) {
+        if (err)
+            return self.invalid().push(err);
+
+        let callback_url = '';
+        for (let i = 0; i < response.settings.length; i++) {
+            if (response.settings[i].name === 'website_url') {
+                callback_url = `${response.settings[i].value}/auth/gitlab/callback`;
+                break;
+            }
+        }
+
+        response.settings = response.settings.concat({
+            name: 'gitlab_oauth2_callback',
+            value: callback_url,
+            isconst: true
+        }).sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);} );
+
+        self.json({
+			label: {
+                count: response.labels.length,
+                items: response.labels
+            },
+            project: {
+                count: response.projects.length,
+                items: response.projects
+			},
+			global: {
+                count: response.settings.length,
+                items: response.settings
+            }
+		});
+    });
+}
+
+function json_save_settings() {
+    let self = this;
+
+    if (!self.user.isadmin)
+        return self.invalid().push('error-privileges');
+
+    self.$save(self, self.callback());
+}
+
+function json_remove_settings(type, name) {
+    let self = this;
+
+    if (!self.user.isadmin)
+        return self.invalid().push('error-privileges');
+
+    switch (type) {
+        case 'global':
+            self.type = 'cdl_settings';
+            break;
+        case 'label':
+            self.type = 'cdl_label';
+            break;
+        case 'project':
+            self.type = 'cdl_project';
+            break;
+        default:
+            return self.invalid().push('error-settings-404');
+    }
+
+    self.name = name;
+    self.$remove(self, self.callback());
+}
+
+function json_external_project() {
+	let self = this;
+
+    if (!self.user.isadmin)
+        return self.invalid().push('error-privileges');
+
+	if (!(self.body.hasOwnProperty('source') && self.body.hasOwnProperty('token') && self.body.source.match(/^[a-z]{2,20}/)))
+        return self.invalid().push('error-privileges');
+
+	let page = self.body.hasOwnProperty('page') && self.body.page.toString().match(/^[0-9]/) ? self.body.page : 1;
+
+    let sql = DB();
+
+    sql.select('project', 'cdl_settings').make(function (builder) {
+    	builder.field('value');
+		builder.where('name', self.body.source);
+		builder.first();
+    });
+
+    Async.waterfall([
+        (callback) => sql.exec((error, response) => callback(error, response)),
+        (data, callback) => Request({
+            method: 'get',
+            url: `${data.project.value.slice(-1) === '/' ? data.project.value.slice(0, -1) : data.project.value}/api/v4/projects?simple=true&page=${page}&per_page=10`,
+            headers: {
+                'private-token': self.body.token
+            },
+            json: true
+        }, (error, response, body) => {
+            if (error)
+                return callback(error);
+
+            if (response.statusCode !== 200)
+                return callback(body);
+
+            callback(null, {
+            	total: response.headers['x-total'],
+				pages: response.headers['x-total-pages'],
+				limit: response.headers['x-per-page'],
+            	body: body
+            });
+        })
+    ], function (error, response) {
+        if (error)
+            return self.invalid().push(error);
+
+        let projects = [];
+        for (let i = 0; i < response.body.length; i++) {
+            // noinspection JSUnresolvedVariable
+            projects.push({
+				name: response.body[i].path_with_namespace,
+				source: self.body.source
+            });
+        }
+
+        self.json({
+			count: response.body.length,
+			items: projects,
+			pages: response.pages,
+			page: page,
+			limit: response.limit,
+			total: response.total
+        });
+    });
 }
 
 function json_query() {
--- controllers/default.js
+++ controllers/default.js
@@ -1,79 +1,219 @@
-exports.install = function() {
-	F.route('/*', 'index', ['authorize']);
-	F.route('/*', 'login', ['unauthorize']);
-	F.route('/logoff', redirect_logoff, ['authorize']);
+/**
+ * @property use
+ * @property initialize
+ * @property authenticate
+ */
+const Passport = require('passport');
+const GitLabStrategy = require('passport-gitlab2').Strategy;
+const Request = require('request');
+const Lo = require('sqlagent/pg-lo');
+const Async = require('async');
 
-	// Files
-	F.file('/download/*',   file_download);
-	F.file('/photos/*.jpg', file_photo);
+let gitlab_service;
 
-	// Templates
-	F.localize('/templates/*.html', ['compress']);
+exports.install = function () {
+    F.route('/*', 'index', ['authorize']);
+    F.route('/*', 'login', ['unauthorize']);
+    F.route('/auth/gitlab', oauth2_gitlab);
+    F.route('/auth/gitlab/callback', oauth2_gitlab_callback);
+    F.route('/logoff', redirect_logoff, ['authorize']);
+
+    F.route('/test', function () {
+        var self = this;
+
+        self.json({ok: self.req.isAuthenticated()});
+    }, ['unauthorize']);
+
+    // Files
+    F.file('/download/*', file_download);
+    F.file('/photos/*.jpg', file_photo);
+
+    // Templates
+    F.localize('/templates/*.html', ['compress']);
 };
+
+function oauth2_gitlab() {
+    let self = this;
+    let sql = DB();
+
+    sql.query('gitlab', `select json_object_agg(name, value) as oauth2 from cdl_settings where name IN ('gitlab', 'gitlab_oauth2_application_id', 'gitlab_oauth2_secret', 'website_url')`)
+        .make(function (builder) {
+            builder.first();
+        });
+
+    sql.exec(function (err, response) {
+        if (err)
+            return console.log(err);
+
+        // noinspection JSUnresolvedVariable
+        gitlab_service = response.gitlab.oauth2.gitlab;
+
+        // noinspection JSUnresolvedVariable
+        Passport.use(new GitLabStrategy({
+                clientID: response.gitlab.oauth2.gitlab_oauth2_application_id,
+                clientSecret: response.gitlab.oauth2.gitlab_oauth2_secret,
+                callbackURL: `${response.gitlab.oauth2.website_url.slice(-1) === '/' ? response.gitlab.oauth2.website_url.slice(0, -1) : response.gitlab.oauth2.website_url}/auth/gitlab/callback`,
+                baseURL: response.gitlab.oauth2.gitlab
+            },
+            (accessToken, refreshToken, profile, callback) => callback(null, profile)
+        ));
+
+        self.custom();
+
+        Passport.initialize();
+        Passport.authenticate('gitlab')(self.req, self.res);
+    });
+}
+
+function oauth2_gitlab_callback() {
+    /**
+     *
+     * @property self.user._json.name
+     * @property self.user._json.username
+     * @property self.user._json.email
+     * @property self.user._json.avatar_url
+     * @property self.user._json.is_admin
+     */
+    let self = this;
+
+    Passport.authenticate('gitlab', {session: false})(self.req, self.res, function (err) {
+        if (err)
+            return self.redirect('/login');
+
+        if (gitlab_service) {
+            /**
+             * Convert hash url with real gitlab address (if use docker for run gitlab)
+             */
+            gitlab_service = gitlab_service.slice(-1) === '/' ? gitlab_service : `${gitlab_service}/`;
+            self.user._json.avatar_url = self.user._json.avatar_url.replace(/^https?:\/\/[^./]+\//, gitlab_service);
+        }
+
+        let index = self.user._json.avatar_url.lastIndexOf('.');
+        let extension = '.png';
+        if (index !== -1)
+            extension = self.user._json.avatar_url.substring(index).toLowerCase();
+
+        GETSCHEMA('Oauth2').make({
+            name: self.user._json.name,
+            username: self.user._json.username,
+            email: self.user._json.email,
+            photo: '',
+            isadmin: self.user._json.is_admin,
+            iscustomer: !self.user._json.is_admin
+        }, function (error, entity) {
+            if (error)
+                return self.json(error);
+
+            entity.$workflow('login', self, function (error, result, user) {
+                if (error)
+                    return self.json(error);
+
+                if (!gitlab_service)
+                    return self.redirect('/');
+
+                let sql = DB();
+                if (user.photo) {
+                    /**
+                     * For connect to pool database
+                     */
+                    sql.query('select 1');
+                    sql.exec(function (error) {
+                        if (error)
+                            return self.json(error);
+
+                        Lo.create(sql.db).unlink(user.photo.split('x')[0], function () {
+                            addImageToDatabase(sql, user.id)
+                        });
+                    });
+                } else
+                    addImageToDatabase(sql, user.id)
+            });
+        });
+
+        function addImageToDatabase(sql, user_id) {
+            sql.writeStream(Request(self.user._json.avatar_url), function (error, oid) {
+                if (error)
+                    return self.json(error);
+
+                sql.query('updateUserPhoto', 'UPDATE tbl_user SET photo = {0} WHERE id = {1}'
+                    .format(sql.escape(HelpDesk.filename(oid, extension).replace(extension, '')), sql.escape(user_id)));
+
+                sql.exec(function (error) {
+                    self.redirect('/');
+
+                    if (error) {
+                        console.log(error.items);
+                        setTimeout(() => Lo.create(sql.db).unlink(oid, error => error && console.log(error)), 300);
+                    }
+                }, 'updateUserPhoto')
+            });
+        }
+    });
+}
 
 // Signs out the user
 function redirect_logoff() {
-	var self = this;
-	delete F.SESSION[self.user.id];
-	self.cookie(CONFIG('auth.cookie'), '', '-1 day');
-	self.redirect('/');
+    var self = this;
+    delete F.SESSION[self.user.id];
+    self.cookie(CONFIG('auth.cookie'), '', '-1 day');
+    self.redirect('/');
 }
 
 // Reads photo from DB
 function file_photo(req, res) {
-	var id = req.split[1].substring(0, req.split[1].length - 4).split('x').first();
-	var token = HelpDesk.filename(id, '.jpg');
+    var id = req.split[1].substring(0, req.split[1].length - 4).split('x').first();
+    var token = HelpDesk.filename(id, '.jpg');
 
-	if (token !== req.split[1]) {
-		res.throw404();
-		return;
-	}
+    if (token !== req.split[1]) {
+        res.throw404();
+        return;
+    }
 
-	F.exists(req, res, 10, function(next, filename) {
-		DB().readStream(id, function(err, stream, size) {
+    F.exists(req, res, 10, function (next, filename) {
+        DB().readStream(id, function (err, stream, size) {
 
-			if (err || !size) {
-				next();
-				return res.throw404();
-			}
+            if (err || !size) {
+                next();
+                return res.throw404();
+            }
 
-			var writer = require('fs').createWriteStream(filename);
+            var writer = require('fs').createWriteStream(filename);
 
-			CLEANUP(writer, function() {
-				res.image(filename, function(image) {
-					image.output('jpg');
-					image.quality(90);
-					image.resize(100, 100);
-					image.minify();
-				}, undefined, next);
-			});
+            CLEANUP(writer, function () {
+                res.image(filename, function (image) {
+                    image.output('jpg');
+                    image.quality(90);
+                    image.resize(100, 100);
+                    image.minify();
+                }, undefined, next);
+            });
 
-			stream.pipe(writer);
-		});
-	});
+            stream.pipe(writer);
+        });
+    });
 }
 
 // Performs file download
 function file_download(req, res) {
-	var id = req.split[1].substring(0, req.split[1].length - 4).split('x').first();
-	var token = HelpDesk.filename(id, '.' + req.extension);
+    var id = req.split[1].substring(0, req.split[1].length - 4).split('x').first();
+    var token = HelpDesk.filename(id, '.' + req.extension);
 
-	if (token !== req.split[1]) {
-		res.throw404();
-		return;
-	}
+    if (token !== req.split[1]) {
+        res.throw404();
+        return;
+    }
 
-	F.exists(req, res, 10, function(next, filename) {
-		DB().readStream(id, function(err, stream, size) {
+    F.exists(req, res, 10, function (next, filename) {
+        DB().readStream(id, function (err, stream, size) {
 
-			if (err || !size) {
-				next();
-				return res.throw404();
-			}
+            if (err || !size) {
+                next();
+                return res.throw404();
+            }
 
-			var writer = require('fs').createWriteStream(filename);
-			CLEANUP(writer, () => res.file(filename));
-			stream.pipe(writer);
-		});
-	});
+            var writer = require('fs').createWriteStream(filename);
+            CLEANUP(writer, () => res.file(filename));
+            stream.pipe(writer);
+        });
+    });
 }
